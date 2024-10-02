6e5b9cb9ae36c7107d2cc155367d26a7e873d41b
Diane Corriette <dcorriette@gmail.com>
2024-10-02 20:24:35 +0100

    protecting auth using auth().protect()

--- nextjs/middleware.ts
+++ nextjs/middleware.ts
@@ -1,12 +1,20 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/", "/pricing"]);
+
+export default clerkMiddleware((auth, request) => {
+  // if a user is not authenticated and they are trying to
+  // access a private route, redirect them to the clerk login page
+  if (!auth().userId && !isPublicRoute(request)) {
+    auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/(api|trpc)(.*)",
   ],
 };
